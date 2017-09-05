86a1f9ffddfc8dcf07124fdd213e80d05d4d9813
Jingwei <lid.guan@gmail.com>
2017-09-05 14:09:31 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if test -z "$MONGO_PASSWD"; then
+    echo "MONGO_PASSWD not defined"
+    exit 1
+fi
+
+(
+echo "setup mongo auth ..."
+auth="-u admin -p $MONGO_PASSWD"
+js="if (!db.getUser('admin')) { db.createUser({ user: 'admin', pwd: '$MONGO_PASSWD', roles: [ {role:'root', db:'admin'},{role: 'readWriteAnyDatabase', db: 'admin' }]}) }"
+until mongo admin --eval "$js" || mongo admin $auth --eval "$js"; do sleep 5; done
+echo "completed auth setup, killing naked mongo"
+killall mongod
+sleep 1
+killall -9 mongod
+) &
+
+echo "start mongodb wihtout auth"
+chown -R mongodb /data/db
+gosu mongodb mongod --bind_ip=127.0.0.1 "$@"
+
+echo "restarting with auth on"
+sleep 5
+exec gosu mongodb mongod --auth "$@"
